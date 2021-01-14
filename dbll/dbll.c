274a85832973f0859f1f93c872195afd2686972c
Abdallah Kokash <akokash@u.rochester.edu>
2021-01-13 23:20:10 -0500

    Fixed dbll

--- dbll/dbll.c
+++ dbll/dbll.c
@@ -1,12 +1,12 @@
 #include <stdlib.h>
 #include "dbll.h"
 
-/* Routines to create and manipulate a doubly-linked list */
-
-/* create a doubly-linked list */
-/* returns an empty list or NULL if memory allocation failed */
 
 
+/*
+In here we create a doubly linked list.
+dbll_Create will return an empty list or NULL if malloc fails.
+*/
 struct dbll *dbll_create()
 {
 	struct dbll *this = malloc(sizeof(struct dbll));
@@ -18,9 +18,14 @@ struct dbll *dbll_create()
  
 }
 
-/* frees all memory associated with a doubly-linked list */
-/* this must also free all memory associated with the linked list nodes */
-/* assumes user data has already been freed */
+
+
+
+/*
+in dbll_free we'll free all memory associated with a doubly linked list. This also 
+frees the memory associated with the nodes. 
+We herea assume that user data has been freed.
+*/
 void dbll_free(struct dbll *list)
 {
 	
@@ -42,21 +47,10 @@ void dbll_free(struct dbll *list)
 
 }
 
-/* iterate over items stored in a doubly-linked list */
-
-/* begin iteration from node start, end iteration at node stop (include stop in iteration) */
-/* the list is traversed using the next pointer of each node */
-
-/* if start is NULL, then start is the first node */
-/* if end is NULL, then end is the last node */
-
-/* at each node, call the user function f with a pointer to the list,
-   a pointer to the current node, and the value of ctx */
-
-/* if f returns 0, stop iteration and return 1 */
-
-/* return 0 if you reached the end of the list without encountering end */
-/* return 1 on successful iteration */
+/*
+dbll_iterate will iterate through the nodes of the list.
+This returns 1 on succesful iteration
+*/
 
 int dbll_iterate(struct dbll *list,
 				 struct llnode *start,
@@ -84,15 +78,12 @@ int dbll_iterate(struct dbll *list,
   return 1;
 }
 
-/* similar to dbll_iterate, except that the list is traversed using
-   the prev pointer of each node (i.e. in the reverse direction).
 
-   Adjust accordingly. For example if start is NULL, then start is the
-   last node in the list.  If end is NULL, then end is the first node
-   in the list.
 
+
+/*
+Similar to dbll_iterate but done in reverse.
 */
-
 int dbll_iterate_reverse(struct dbll *list,
 						 struct llnode *start,
 						 struct llnode *end,
@@ -112,9 +103,12 @@ int dbll_iterate_reverse(struct dbll *list,
 }
 
 
-/* Remove `llnode` from `list` zz*/
-/* Memory associated with `node` must be freed */
-/* You can assume user_data will be freed by somebody else (or has already been freed) */
+
+
+
+/*
+This removes the llnode from the list given. After we remove we free the memory and assume user data has already been freed.
+*/
 void dbll_remove(struct dbll *list, struct llnode *node){
 
   
@@ -140,10 +134,13 @@ void dbll_remove(struct dbll *list, struct llnode *node){
   
 }
 
-/* Create and return a new node containing `user_data` */
-/* The new node must be inserted after `node` */
-/* if node is NULL, then insert the node at the end of the list */
-/* return NULL if memory could not be allocated */
+
+
+
+
+/*
+Insert a node after a certain node
+*/
 struct llnode *dbll_insert_after(struct dbll *list, struct llnode *node, void *user_data)
 {
 	struct llnode *node2 = malloc(sizeof(struct llnode));
@@ -181,10 +178,13 @@ struct llnode *dbll_insert_after(struct dbll *list, struct llnode *node, void *u
  
 }
 
-/* Create and return a new node containing `user_data` */
-/* The new node must be inserted before `node` */
-/* if node is NULL, then insert the new node at the beginning of the list */
-/* return NULL if memory could not be allocated */
+
+
+
+
+/*
+insert a node before a certain node.
+*/
 struct llnode *dbll_insert_before(struct dbll *list, struct llnode *node, void *user_data){
 
 	struct llnode *node2 = malloc(sizeof(struct llnode));
@@ -227,10 +227,10 @@ struct llnode *dbll_insert_before(struct dbll *list, struct llnode *node, void *
   return node2;
 }
 
-/* create and return an `llnode` that stores `user_data` */
-/* the `llnode` is added to the end of list */
-/* return NULL if memory could not be allocated */
-/* this function is a convenience function and can use the dbll_insert_after function */
+
+/*
+Append a node at the end of the list
+*/
 struct llnode *dbll_append(struct dbll *list, void *user_data){
 
 	struct llnode *node = malloc(sizeof(struct llnode));
